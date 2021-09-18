60aea27adb23d565e59dab331b662c19b6534d21
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2021-09-18 14:05:45 -0500

    Versión inicial
    Se unió Time y Score.

---
+++ Time.java
@@ -0,0 +1,50 @@
+public class Time
+{
+    public void TimeKeeper(int min, int sec)//a class to keep track of the total seconds and minuntes the player has used to get to a level
+	{ 
+	   if(sec + seconds <=60)
+	    {
+	        minutes+=min;
+	        seconds=sec+seconds;
+    	}
+	   else
+	   {
+	       minutes+=min;
+	       minutes+=1*((sec+seconds)/60);
+	       seconds=(sec+seconds)%60;
+	   }
+	}//end TimeKeeper
+	
+
+    public void calcTimeforMaze(int totalDimonds, int xSize, int ySize)//some kinda method that determines the time a player has for each level based on the level size and dimonds.
+    {
+        if(xSize/ySize < 1)//this method should be changed in order to provide a more relistic time system.
+        {
+            minutes+=(ySize/xSize)*1+1;
+        }
+        else
+            minutes+=(ySize/xSize)*1+1;
+        if(totalDimonds >6 && totalDimonds*.10 + seconds <= 60)
+            minutes+=(ySize/xSize)*1+1;
+        else
+        {
+            minutes+=1;          
+        }
+        if(minutes ==0)
+           minutes=2;
+     }//end method
+    
+
+	public int getMinutes()
+	{
+	    return minutes;
+	}
+	
+	public int getSeconds()
+	{
+	    return seconds;
+	}
+	
+int minutes=0; 
+int seconds=0;
+}//end class
